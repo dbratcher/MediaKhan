2fb3851e14f017424c3b4cd7a46dbf2be89357ad
Drew Bratcher <drew@crowdspots.com>
2013-10-14 18:42:37 -0400

    bdb 1000 runs

--- standard_run.pl
+++ standard_run.pl
@@ -1,6 +1,5 @@
- for ($count = 1; $count < 4; $count++) {
-    $out = `rm my_db.db`;
+ for ($count = 2; $count < 4; $count++) {
     $out = `./khan ./test -s`;
     $out = `for f in test/location/laptop/*.mp3; do mv "\$f" test/tags/favs$count/; done`;
-    $out = `mv rename_times.txt measurements/inserts/local/bdb-4000-run$count.txt`;
+    $out = `mv rename_times.txt measurements/inserts/local/vold-4000-run$count.txt`;
  }

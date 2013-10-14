a65e658858c0369c3453e87b0bffdbf1f8397502
Drew Bratcher <drew@crowdspots.com>
2013-10-14 18:38:48 -0400

    bdb 4000 runs

--- standard_run.pl
+++ standard_run.pl
@@ -1,6 +1,6 @@
  for ($count = 1; $count < 4; $count++) {
-    $out = `redis-cli flushall`;
+    $out = `rm my_db.db`;
     $out = `./khan ./test -s`;
-    $out = `for f in test/\@laptop/*.mp3; do mv "\$f" test/\@favs$count/; done`;
-    $out = `mv rename_times.txt measurements/selectors/selector/redis-100-run$count.txt`;
+    $out = `for f in test/location/laptop/*.mp3; do mv "\$f" test/tags/favs$count/; done`;
+    $out = `mv rename_times.txt measurements/inserts/local/bdb-4000-run$count.txt`;
  }

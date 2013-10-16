b5cb52271fa64f9413eefba18ba5247650299ef3
Drew Bratcher <drewmbee@gmail.com>
2013-10-15 22:47:54 -0400

    added ping pong for clock deltas

---
+++ ping.pl
@@ -0,0 +1,43 @@
+use Time::HiRes qw(gettimeofday);
+use IO::Socket::INET;
+ 
+# auto-flush on socket
+$| = 1;
+ 
+# create a connecting socket
+my $socket = new IO::Socket::INET (
+    PeerHost => '10.0.1.4',
+    PeerPort => '7777',
+    Proto => 'tcp',
+);
+die "cannot connect to the server $!\n" unless $socket;
+print "connected to the server\n";
+
+$diff1 = 0;
+$diff2 = 0;
+$num_runs = 1000; 
+# data to send to a server
+for($run=0; $run < $num_runs; $run++) {
+  my $time2 = "";
+  #print "run $run\n";
+  ($seconds, $micro) = gettimeofday;
+  $time1 = $seconds + $micro/1000000.0;
+  $socket->send($time1);
+  # receive a response of up to 1024 characters from server
+  $socket->recv($time2, 1024);
+  #print "recieved $time2\n";
+  ($seconds, $micro) = gettimeofday;
+  $time3 = $seconds + $micro/1000000.0;
+  $diff1 += $time2 - $time1;
+  $diff2 += $time3 - $time2;
+  #print "diff1: $diff1\n";
+  #print "diff2: $diff2\n\n";
+}
+$socket->send("end");
+shutdown($socket, 1);
+
+$diff1/=$num_runs;
+$diff2/=$num_runs;
+print "diff1: $diff1\n";
+print "diff2: $diff2\n";
+$socket->close();
---
+++ pong.pl
@@ -0,0 +1,41 @@
+use Time::HiRes qw(gettimeofday);
+use IO::Socket::INET;
+ 
+# auto-flush on socket
+$| = 1;
+ 
+# creating a listening socket
+my $socket = new IO::Socket::INET (
+    LocalHost => '10.0.1.4',
+    LocalPort => '7777',
+    Proto => 'tcp',
+    Listen => 5,
+    Reuse => 1
+);
+die "cannot create socket $!\n" unless $socket;
+print "server waiting for client connection on port 7777\n";
+ 
+while(1)
+{
+    # waiting for a new client connection
+    my $client_socket = $socket->accept();
+ 
+    # get information about a newly connected client
+    my $client_address = $client_socket->peerhost();
+    my $client_port = $client_socket->peerport();
+    print "connection from $client_address:$client_port\n";
+ 
+    # read up to 1024 characters from the connected client
+    my $data = "";
+    while($data ne "end") {
+      $client_socket->recv($data, 1024);
+      #print "data: $data\n";
+      ($seconds, $micro) = gettimeofday;    
+      $time2 = $seconds + $micro/1000000.0; 
+      $client_socket->send($time2);
+      # notify client that response has been sent
+    }
+    shutdown($client_socket, 1);
+}
+ 
+$socket->close();

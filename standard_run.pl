34ad9b147eec8f3255948551caedf876209832db
Drew Bratcher <drew@crowdspots.com>
2013-10-16 10:02:46 -0400

    more redis selector runs

--- standard_run.pl
+++ standard_run.pl
@@ -1,5 +1,5 @@
- for ($count = 2; $count < 4; $count++) {
+ for ($count = 1; $count < 4; $count++) {
     $out = `./khan ./test -s`;
     $out = `for f in test/location/laptop/*.mp3; do mv "\$f" test/tags/favs$count/; done`;
-    $out = `mv rename_times.txt measurements/selectors/path/redis-1000-run$count.txt`;
+    $out = `mv rename_times.txt measurements/selectors/selector/redis-4000-run$count.txt`;
  }
